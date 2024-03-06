633441875cd28dfc2f033f5f1862de19f4d7a97e
Nick Miller <nick@evvnt.com>
2024-03-06 09:57:37 -0500

    Supply default text_field attributes

--- lib/coprl/presenters/plugins/phone_number_field/components/phone_number_field_component.rb
+++ lib/coprl/presenters/plugins/phone_number_field/components/phone_number_field_component.rb
@@ -4,6 +4,10 @@ module Coprl
       module PhoneNumberField
         class PhoneNumberFieldComponent < DSL::Components::TextField
           def initialize(**attributes, &block)
+            attributes = {
+              behavior: :tel,
+              auto_complete: "tel-national"
+            }.merge(attributes)
             super(type: :phone_number_field, **attributes, &block)
             expand!
           end
